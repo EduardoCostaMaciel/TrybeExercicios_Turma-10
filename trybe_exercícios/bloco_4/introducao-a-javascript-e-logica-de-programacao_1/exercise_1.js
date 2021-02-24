34faae639d3e48ceaa4d9757ed8daad1a4681478
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-02-24 20:49:36 -0300

    Cria um programa que compara tres variaveis, qual tem o valor maior usando if e else

--- trybe_exercícios/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
+++ trybe_exercícios/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -16,3 +16,16 @@ let maiorQueOutro = 30;
     }else{
         console.log(0);
     }
+/*3-Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.(ok)*/
+let maiorQa = 30;
+let maiorQb = 100;
+let maiorQc = 80;
+    if(maiorQa > maiorQb && maiorQa > maiorQc) {
+        console.log(maiorQa);
+    }else if (maiorQb > maiorQa && maiorQb > maiorQc) {
+        console.log(maiorQb);
+    }else if(maiorQc > maiorQa && maiorQc > maiorQb) {
+        console.log(maiorQc);
+    }else {
+        console.log(0);
+    }
